e34c8621e03f798d20a7e8d0a2433c11f63b4c52
comboyin <trannhut031192@gmail.com>
2015-10-27 22:01:39 +0700

    my home

--- lession3/index.php
+++ lession3/index.php
@@ -13,9 +13,8 @@
 */
  
   define ( '__DOMAIN' , 'http://localhost' );
-  define ('__FOLDER', '/lampart/testProject/testProject/lession3/');
+  define ('__FOLDER', '/lampart/testProject/lession3/');
   
- 
  define ('__FOLDER_UPLOADS', 'uploads');
 
  /*** include the config.php file ***/
@@ -27,25 +26,7 @@
  include  __SITE_PATH.'/simple-php-captcha/simple-php-captcha.php';
 
  /*** include the entity file ***/
-/*  function __autoload($class){
- 	if(file_exists(__SITE_PATH . '/' . $class .".php")){
- 		require(__SITE_PATH . '/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/base/' . $class.".php")){
- 		require(__SITE_PATH . '/base/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/controllers/' . $class.".php")){
- 		require(__SITE_PATH . '/controllers/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/config/' . $class.".php")){
- 		require(__SITE_PATH . '/config/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/models/' . $class.".php")){
- 		require(__SITE_PATH . '/models/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/utility/' . $class.".php")){
- 		require(__SITE_PATH . '/utility/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/class/' . $class.".php")){
- 		require(__SITE_PATH . '/class/' . $class.".php");
- 	}else if(file_exists(__SITE_PATH . '/entity/' . $class.".php")){
- 		require(__SITE_PATH . '/entity/' . $class.".php");
- 	}
- } */
+
 
  function __autoload($class){
  	if(file_exists(__SITE_PATH . '/' . $class .".php")){
@@ -66,28 +47,6 @@
  		require_once(__SITE_PATH . '/entity/' . $class.".php");
  	}
  }
-
-
- /*** include the entity file ***/
- /* function __autoload($class){
- 	if(file_exists(__SITE_PATH . '/' . $class .".php")){
- 		include_once __SITE_PATH . '/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/base/' . $class.".php")){
- 		include_once __SITE_PATH . '/base/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/controllers/' . $class.".php")){
- 		include_once __SITE_PATH . '/controllers/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/config/' . $class.".php")){
- 		include_once __SITE_PATH . '/config/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/models/' . $class.".php")){
- 		include_once __SITE_PATH . '/models/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/utility/' . $class.".php")){
- 		include_once __SITE_PATH . '/utility/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/class/' . $class.".php")){
- 		include_once __SITE_PATH . '/class/' . $class.".php";
- 	}else if(file_exists(__SITE_PATH . '/entity/' . $class.".php")){
- 		include_once __SITE_PATH . '/entity/' . $class.".php";
- 	}
- } */
 
  /*** a new registry object ***/
  $registry = new Registry($config);
